c8343909dfb8272950a63c38b97728410105c6e8
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2020-02-02 02:06:25 +0900

    画像をカーソル下に表示する機能を実装

--- index.js
+++ index.js
@@ -1,98 +1,146 @@
-// class ImageView extends React.Component {
-//     render () {
-//         return (
-//             <div>hoge</div>
-//         )
-//     }
-// }
-
 exports.decorateTerm = (Term, { React, notify }) => {
     return class extends React.Component {
         constructor(props, context) {
           super(props, context);
-          this.term = null;
+          this._term = null;
+          this._cursorFrame = null
           this.onDecorated = this.onDecorated.bind(this);
+          this.onCursorMove = this.onCursorMove.bind(this);
         }
 
         onDecorated(term) {
-            console.log("はいいいいいいいいいいいいい");
-            console.log(term);
-            this.term = term;
-            // Don't forget to propagate it to HOC chain
+            this._term = term;
             if (this.props.onDecorated) this.props.onDecorated(term);
+            if(this._term.termRef !== null) {
+                this._term.termRef.addEventListener(
+                    'keyup', event => this.handleKeyUp(event),
+                    false
+               );
+            }
+        }
+
+        handleKeyUp(event) {
+            const {keyCode} = event;
+            if(keyCode === 13) { // ENTER
+                return;
+            } else if (keyCode === 8) { // BACKSPACE
+                store.dispatch({
+                    type: 'HOOK_COMMAND',
+                    message: '',
+                });
+            } else if (keyCode === 67) { // Ctrl+C
+            }
+        }
+
+        onCursorMove (cursorFrame) {
+          if (this.props.onCursorMove) this.props.onCursorMove(cursorFrame);
+          this._cursorFrame = cursorFrame;
+        }
+
+        custom(term) {
+            if(term == null) return;
+            let imgView = React.createElement('div',null,'hoge');
+
+            this._container = term.termRef;
+            this._xTermScreen = this._container.querySelector('.xterm .xterm-screen');
+            const renderLayers = Array.from(this._xTermScreen.querySelectorAll('canvas'));
+
+            var para = document.createElement("div");
+            para.style.cssText = "background-color: rgba(255,0,0,0.3);"
+            var t = document.createTextNode("hogheohge");
+            para.appendChild(t);
+            // renderLayers[1].append(para)
+            // renderLayers[2].style.cssText = "background-color: rgba(255,0,0,0.3);"
+            for (const canvas of renderLayers) {
+                // console.log(canvas);
+                canvas.style.opacity = 1.0;
+            }
         }
 
         render () {
-            // console.log("render");
-            // console.log(this.props);
-            // // const imgView = this.props.myState.message !== '' ?
-            //     // React.createElement('img', {src: '/Users/rasukaru/Desktop/320x240.png'}) : null;
-            //
-            // // const imgView = React.createElement('div',null,'hoge');
-            let imgView = null;
-            let height = '80%';
-            let message = 'message' in this.props.myState ? this.props.myState.message : ''; 
-            if (message !== '') {
-                console.log("きたね");
-                imgView = React.createElement('div',null,'hoge');
-                // height = '80%';
+            console.log("render");
+            if (this.props.myState === undefined) {
+                return React.createElement( Term, Object.assign({}, this.props));
             }
-            // var para = document.createElement("div");
-            // para.style.cssText = "background-color: rgba(255,0,0,0.3);"
-            // var t = document.createTextNode("hogheohge");
-            // para.appendChild(t);
-            // // document.getElementsByClassName("xterm-screen")[0].appendChild(para); 
-            //
+            // console.log(this._term);
+            // this.custom(this._term);
+            let imgView = null;
+            if (this.props.myState.message !== '' && this._cursorFrame !== null) {
+                console.log("きたね");
+                // console.log(this.props.myState);
+                const { x, y } = this._cursorFrame;
+                const origin = this._term.termRef.getBoundingClientRect();
+                imgView = React.createElement(
+                    'img', {
+                    style: {
+                        position: 'absolute',
+                        top: y + origin.top + 15,
+                        left: 0,
+                    },
+                    src: '/Users/rasukaru/Desktop/320x240.png',
+                    }
+                );
+            }
+
             const children = [
-                React.createElement(Term, this.props),
+                React.createElement(
+                    Term,
+                    Object.assign({}, this.props, {
+                        onDecorated: this.onDecorated,
+                        onCursorMove: this.onCursorMove,
+                    })),
                 imgView,
             ];
 
-            // const children = [React.createElement(
-            //     Term,
-            //     Object.assign({}, this.props, {
-            //       onDecorated: this.onDecorated,
-            //     }),
-            //     imgView
-            //   )];
-            //
-            return React.createElement('div', {style: { width: '100%', height: height, position: 'relative'}}, children)
-            // return (
-            //     <div>
-            //         React.createElement(Term, Object.assign({}, this.props))
-            //     </div>
-            // )
-            // return React.createElement('div', null, children)
+            return React.createElement(
+                'div',
+                {style: { 
+                    width: '100%', 
+                    height: '100%', 
+                    position: 'relative'}
+                },
+                children
+            )
         }
     }
 }
 
 exports.middleware = store => next => (action) => {
+    // console.log(action);
+    if (action.type === 'UPDATE_QUERY') {
+        console.log("っっっっっっっっっっk");
+        console.log(action);
+    }
     if (action.type === 'SESSION_ADD_DATA') {
-        if (action.data.match(/zsh: command not found: .*/g)) {
+        const { data } = action;
+        if (detectImgCatCommand(data)) {
             store.dispatch({
                 type: 'HOOK_COMMAND',
                 message: 'what happen!?',
             });
         } else {
-            store.dispatch({
-                type: 'HOOK_COMMAND',
-                message: '',
-            });
-
+            // store.dispatch({
+            //     type: 'HOOK_COMMAND',
+            //     message: '',
+            // });
+            next(action);
         }
+    }else {
+        next(action);
     }
-    next(action);
 }
 
 exports.reduceUI = (state, action) => {
     switch (action.type) {
         case 'HOOK_COMMAND':
-            const { message } = action;
-            return state.set('myState', { message });
-        default:
-            return state;
+            if (state.myState === undefined) {
+                return state.set('myState', {message: ''});
+            }
+            return state.set('myState', {
+                message : action.message,
+            });
     }
+    return state;
 };
 
 exports.mapTermsState = (state, map) => Object.assign(map, {
@@ -111,4 +159,15 @@ exports.decorateConfig = (config) => {
         // cursorColor: 'blue',
         cursorColor: 'yellow',
     })
+}
+
+function detectImgCatCommand(data) {
+    const patterns = [
+        'zsh: command not found',
+        'imgcat: command not found',
+        'command not found: imgcat',
+        'Unknown command \'imgcat\'',
+        '\'imgcat\' is not recognized*',
+    ];
+  return new RegExp('(' + patterns.join(')|(') + ')').test(data)
 }
